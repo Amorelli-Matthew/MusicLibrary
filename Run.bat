104e4a5d88c54a5f6bb8eb144706fa16c512d1d4
Matthew Amorelli <77508333+Amorelli-Matthew@users.noreply.github.com>
2025-08-09 21:09:41 -0700

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,6 +1,18 @@
 echo Running MusicLibraryApplication...
+
+setlocal enabledelayedexpansion
+
+REM Set the base path where JavaFX SDK folders are stored
+set JAVAFX_BASE=C:\javafx
+
+REM Find the first folder in the base path that starts with "javafx-sdk"
+for /d %%i in ("%JAVAFX_BASE%\javafx-sdk*") do (
+    set JAVAFX_DIR=%%i
+    goto found
+)
+
 cd out
-java --module-path "%USERPROFILE%\Documents\javafx-sdk-24.0.1\lib" ^
+java --module-path "%USERPROFILE%\Documents\%JAVAFX_DIR%\lib" ^
      --add-modules javafx.controls,javafx.fxml ^
      -cp . MusicLibraryApplication.MusicLibraryApplication
 
